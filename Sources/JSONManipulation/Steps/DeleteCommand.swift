38eb273a88668068ed7f16a695f5b9992ae247a8
baguio <1548170+baguio@users.noreply.github.com>
2020-04-21 22:28:14 -0500

    Delete: Updated to follow new RawInputParameters protocol
    Also now it can delete any input

--- Sources/JSONManipulation/Steps/DeleteCommand.swift
+++ Sources/JSONManipulation/Steps/DeleteCommand.swift
@@ -6,12 +6,20 @@ import JSONKit
 struct Delete: StepParams {}
 
 extension Delete: RawInputParameters {
-    func process(fileOrFolder: FileOrFolder) throws {
-        switch fileOrFolder {
-        case .file(let file):
+    func process(input: StepIO, storage: ProcessorStorage) throws {
+        switch input {
+        case .file(let fileParams):
+            let file = try storage.baseFolder.file(at: fileParams.file)
             try file.delete()
-        case.folder(let folder):
+        case .folder(let folderParams):
+            let folder = try storage.baseFolder.subfolder(at: folderParams.folder)
             try folder.delete()
+        case .named(let namedParams):
+            storage.clearNamed(namedParams.name)
+        case .multiple(let steps):
+            try steps.forEach {
+                try process(input: $0, storage: storage)
+            }
         }
     }
 }
