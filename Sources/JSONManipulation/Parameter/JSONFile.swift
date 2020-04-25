5c4941877e1d20e928629765c12edf61565c6aeb
baguio <1548170+baguio@users.noreply.github.com>
2020-04-24 21:02:19 -0500

    JSONFile: A File parse result
    Contains its json content plus additional keys. By default it has its original filename (excluding extension)

---
+++ Sources/JSONManipulation/Parameter/JSONFile.swift
@@ -0,0 +1,33 @@
+//
+//  File.swift
+//  
+//
+//  Created by everis on 4/22/20.
+//
+
+import Foundation
+import JSONKit
+import Files
+
+extension File {
+    func readJSON() throws -> JSONFile {
+        let data = try self.read()
+        let json = try JSONDecoder().decode(JSONNode.self, from: data)
+        let jsonFile = JSONFile(json: json)
+        jsonFile.extra["name"] = self.nameExcludingExtension
+        return jsonFile
+    }
+    
+    func writeJSON(_ jsonFile: JSONFile) throws {
+        let data = try JSONEncoder().encode(jsonFile.json)
+        try write(data)
+    }
+}
+
+class JSONFile {
+    var extra = [String: String]()
+    var json: JSONNode
+    init(json: JSONNode) {
+        self.json = json
+    }
+}
