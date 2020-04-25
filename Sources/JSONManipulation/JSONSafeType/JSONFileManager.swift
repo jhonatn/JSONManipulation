30ac7df88610cf7fa02211d5126c8704e7feb56e
baguio <1548170+baguio@users.noreply.github.com>
2020-04-24 21:09:43 -0500

    Removed unused JSON parsing methods

--- Sources/JSONManipulation/JSONSafeType/JSONFileManager.swift
+++ Sources/JSONManipulation/JSONSafeType/JSONFileManager.swift
@@ -1,24 +1,5 @@
 import Foundation
-import Files
 import JSONKit
-
-extension File {
-    func writeJSON(_ json: JSONNode) throws {
-        let data = try JSONEncoder().encode(json)
-        try write(data)
-    }
-    
-    func readJSON() throws -> JSONNode {
-        let data = try self.read()
-        return try JSONDecoder().decode(JSONNode.self, from: data)
-    }
-    
-    func editJSON(_ handler: (JSONNode) throws -> JSONNode) throws {
-        let json = try readJSON()
-        let newJson = try handler(json)
-        try writeJSON(newJson)
-    }
-}
 
 extension Array where Element: Hashable {
     func removingDuplicates() -> [Element] {
